f27130ed6b9c8c8f7ad66ec14ba08da0a9b5c090
Nigel D'Souza <nigeldsouza@outlook.com>
2017-09-06 12:58:33 -0500

    Exit after shell change

--- after.sh
+++ after.sh
@@ -2,6 +2,7 @@
 
 # Switch shell
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+exit
 
 # If tmux isn't displaying symbols correctly
 echo "export LC_ALL=en_US.UTF-8" >> ~/.zshrc
