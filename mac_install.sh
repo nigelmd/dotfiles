c64bd53d2a2a823a11989c6d2c19a15d77d26bb3
Nigel D'Souza <nigeldsouza@outlook.com>
2025-09-21 12:03:20 -0700

    Move all ~/.configs under ~/.config, add starship and sesh configs, modify mac script

--- mac_install.sh
+++ mac_install.sh
@@ -4,17 +4,29 @@
 npm install -g @mermaid-js/mermaid-cli
 
 # For colorscripts which is used by the LazyVim Github dashboard
-# Not using anymore since switched to nushell
-#git clone https://gitlab.com/dwt1/shell-color-scripts.git
-#cd shell-color-scripts
-#sudo make install
+cd ~/
+git clone https://gitlab.com/dwt1/shell-color-scripts.git
+cd shell-color-scripts
+sudo make install
 
-# TODO
-# Add cloning nuscripts to ~/.config dir
+# TODO: add cloning nuscripts to ~/.config dir
 
-# Copy starship config
+# copy all configs to home directory
+mkdir -p ~/.config
+cp -r .config ~/
+
+# copy starship config
 mkdir ($nu.data-dir | path join "vendor/autoload")
 starship init nu | save -f ($nu.data-dir | path join "vendor/autoload/starship.nu")
+
+# copy wezterm config
+cp .wezterm.lua ~/
+
+# copy tmux config
+cp .tmux.conf ~/
+
+# copy nushell config to correct path
+cp config.nu ~/Library/Application Support/nushell/
 
 # once nushell is installed and carapace
 ## ~/.config/nushell/env.nu
