5bc51b4d9a3d6ef835d8a6d07c9662cbcfe9e716
Nigel D'Souza <nigeldsouza@outlook.com>
2026-02-10 11:30:14 -0800

    Fix copy instructions

--- mac_install.sh
+++ mac_install.sh
@@ -1,32 +1,39 @@
 #!/bin/bash
 
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # For mermaid diagrams which is used by LazyVim
-npm install -g @mermaid-js/mermaid-cli
+if command -v npm &> /dev/null; then
+  npm install -g @mermaid-js/mermaid-cli
+else
+  echo "npm not found, skipping mermaid-cli install"
+fi
 
 # For colorscripts which is used by the LazyVim Github dashboard
-cd ~/
-git clone https://gitlab.com/dwt1/shell-color-scripts.git
-cd shell-color-scripts
+if [ ! -d "$HOME/shell-color-scripts" ]; then
+  git clone https://gitlab.com/dwt1/shell-color-scripts.git "$HOME/shell-color-scripts"
+fi
+cd "$HOME/shell-color-scripts"
 sudo make install
 
 # TODO: add cloning nuscripts to ~/.config dir
 
 # copy all configs to home directory
 mkdir -p ~/.config
-cp -r .config ~/
+cp -r "$DOTFILES_DIR/.config" ~/
 
 # copy starship config
 mkdir -p "$HOME/Library/Application Support/nushell/vendor/autoload"
 starship init nu > "$HOME/Library/Application Support/nushell/vendor/autoload/starship.nu"
 
 # copy wezterm config
-cp .wezterm.lua ~/
+cp "$DOTFILES_DIR/.wezterm.lua" ~/
 
 # copy tmux config
-cp .tmux.conf ~/
+cp "$DOTFILES_DIR/.tmux.conf" ~/
 
 # copy nushell config to correct path
-cp config.nu ~/Library/Application Support/nushell/
+cp "$DOTFILES_DIR/config.nu" "$HOME/Library/Application Support/nushell/"
 
 # once nushell is installed and carapace
 ## ~/.config/nushell/env.nu
